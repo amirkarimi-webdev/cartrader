ed339deedd9bcf3aea8673cdc473c68f4b713b8f
amir karimi <amirsmylife@gmail.com>
2026-01-01 16:39:37 +0330

    and now we can see messages

--- components/Car/MessageCard.vue
+++ components/Car/MessageCard.vue
@@ -1,14 +1,18 @@
+<script setup>
+defineProps({
+  message: Object,
+});
+</script>
+
 <template>
   <div class="even:bg-gray-100 p-4">
-    <h1 class="font-bold text-2xl">laith harb</h1>
+    <h1 class="font-bold text-2xl">{{ message.name }}</h1>
     <p class="text-gray-700">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ab
-      necessitatibus consequuntur quae. Maxime assumenda libero, totam est
-      beatae odit.
+      {{ message.message }}
     </p>
     <div class="flex items-center mt-4">
-      <p class="text-green-700 mr-10">a@email.com</p>
-      <p class="text-blue-500">555-555-555</p>
+      <p class="text-green-700 mr-10">{{message.email}}</p>
+      <p class="text-blue-500">{{ message.phone }}</p>
     </div>
   </div>
 </template>
